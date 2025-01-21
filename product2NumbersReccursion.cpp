044383d6a27fa9ff70d0f3e7e42a2207adab01e4
Suraj <surajyadav052005@gmail.com>
2025-01-21 12:19:57 +0530

    product using recurssion

---
+++ product2NumbersReccursion.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int productOfNumber(int a, int b)
+{
+    if(b==1)
+        return a;
+
+    return a + productOfNumber(a, b-1);
+}
+
+int main()
+{
+    int a, b;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    int result = productOfNumber(a, b);
+    cout << "Product is: " << result << endl;
+
+    return 0;
+}
